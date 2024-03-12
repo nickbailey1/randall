161ec4ef5598d604c5bd3fd34ba69f1f182b5baf
Nick Bailey <nickbailey@g.ucla.edu>
2024-03-11 22:39:13 -0700

    updated options.h for clarity

--- options.h
+++ options.h
@@ -1,13 +1,13 @@
-enum Input { BLANK, RDRAND, LRAND48_R, SLASH_F}
-enum Output { STDOUT, N }
+enum Input { NONE, RDRAND, LRAND48_R, FILE} // enumerate options regarding input
+enum Output { STDIO, N } // enumerate options regarding output
 
 struct options {
-    bool isValid; // true if all opts are valid, false if not
-    long long nbytes;
+    bool is_valid; // true if all options are valid, false if not
+    long long num_input_bytes;
     enum Input input;
-    char* r_src;
+    char* rand_src_filename;
     enum Output output;
-    unsigned int block_size;
+    unsigned int output_block_size;
 };
 
-void readopt(int argc, char **argv, struct options* options);
+void read_options(int argc, char **argv, struct options* options);
