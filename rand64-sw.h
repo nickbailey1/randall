0fcee195e9adc6a7d0410f1b36bfb29c2e76f41f
Nick Bailey <nickbailey@g.ucla.edu>
2024-03-11 21:53:29 -0700

    set up rand64-sw.h header file

---
+++ rand64-sw.h
@@ -0,0 +1,7 @@
+/* this header file defines the functions implemented in rand64-sw.c */
+
+void software_rand64_init(void);
+
+unsigned long long software_rand64(void);
+
+unsigned long long software_rand64_fini(void);
