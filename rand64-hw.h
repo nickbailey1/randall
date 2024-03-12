51c20c559bd55d5854e3245ae1c6d65eebd67cec
Nick Bailey <nickbailey@g.ucla.edu>
2024-03-11 21:49:35 -0700

    wrote rand64-hw.h header file that defines functions implemented in rand64-hw.c

--- rand64-hw.h
+++ rand64-hw.h
@@ -1,59 +1,11 @@
-#include <immintrin.h>
-#include <cpuid.h>
-#include <time.h>
-#include <stdio.h>
-#include <errno.h>
+/* this header file defines the functions implemented in rand64-hw.c */
 
-#include "rand64-hw.h"
+_Bool rdrand_supported(void);
 
-/* Hardware implementation.  */
+void hardware_rand64_init(void);
 
-/* Description of the current CPU.  */
-struct cpuid { unsigned eax, ebx, ecx, edx; };
+unsigned long long hardware_rand64();
 
-/* Return information about the CPU.  See <http://wiki.osdev.org/CPUID>.  */
-struct cpuid cpuid (unsigned int leaf, unsigned int subleaf)
-{
-    struct cpuid result;
-    asm ("cpuid"
-       : "=a" (result.eax), "=b" (result.ebx),
-     "=c" (result.ecx), "=d" (result.edx)
-       : "a" (leaf), "c" (subleaf));
-    return result;
-}
+unsigned long long hardware_lrand48();
 
-/* Return true if the CPU supports the RDRAND instruction.  */
-_Bool rdrand_supported (void)
-{
-    struct cpuid extended = cpuid (1, 0);
-    return (extended.ecx & bit_RDRND) != 0;
-}
-
-struct drand48_data drand_buf;
-
-/* Initialize the hardware rand64 implementation.  */
-void hardware_rand64_init (void)
-{
-    srand48_r(time(NULL), &drand_buf);
-}
-
-/* Return a random value, using hardware operations.  */
-unsigned long long hardware_rand64 ()
-{
-    unsigned long long int x;
-    while (! _rdrand64_step (&x))
-        continue;
-    return x;
-}
-
-unsigned long long hardware_mrand48 ()
-{
-    long int result;
-    mrand48_r(&drand_buf, &result);
-    return (unsigned long long) result;
-}
-
-/* Finalize the hardware rand64 implementation.  */
-void hardware_rand64_fini (void)
-{
-}
+void hardware_rand64_fini(void);
