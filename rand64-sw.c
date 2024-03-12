c0532546acb5ebc59519d61312f0ddf224772079
Nick Bailey <nickbailey@g.ucla.edu>
2024-03-11 21:55:10 -0700

    copied and pasted software functions from randall.c into rand64-sw.c and added necessary include statements

---
+++ rand64-sw.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "rand64-sw.h"
+
+/* Software implementation.  */
+
+/* Input stream containing random bytes.  */
+static FILE *urandstream;
+
+/* Initialize the software rand64 implementation.  */
+static void
+software_rand64_init (void)
+{
+  urandstream = fopen ("/dev/random", "r");
+  if (! urandstream)
+    abort ();
+}
+
+/* Return a random value, using software operations.  */
+static unsigned long long
+software_rand64 (void)
+{
+  unsigned long long int x;
+  if (fread (&x, sizeof x, 1, urandstream) != 1)
+    abort ();
+  return x;
+}
+
+/* Finalize the software rand64 implementation.  */
+static void
+software_rand64_fini (void)
+{
+  fclose (urandstream);
+}
