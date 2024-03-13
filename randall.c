2898523f47065cbab944373b685d20b5a7bf4d14
Nick Bailey <nickbailey@g.ucla.edu>
2024-03-12 18:02:13 -0700

    wrote code for determining randomness function based on options

--- randall.c
+++ randall.c
@@ -36,6 +36,88 @@
 /* Main program, which outputs N bytes of random data.  */
 int main (int argc, char **argv)
 {
+  struct options options;
+  parse_option(argc, argv, &options);
+
+  // first check that options are valid
+  if (!options.valid) {
+    fprintf(stderr, "Invalid options statement.")
+    return 1;
+  }
+  long long num_output_bytes = options.num_output_bytes;
+  
+  /* set up function pointers that can be either the hardware/software functions */
+  void (*initialize) (void);
+  unsigned long long (*rand64) ();
+  void (*finalize) (void);
+
+  /* look at options and decide how to operate */
+  if (options.input == NONE) {
+    /* spec says to use RDRAND as the default and fail if hardware rand is not available */
+    if (rdrand_supported()) {
+      initialize = hardware_rand64_init;
+      rand64 = hardware_rand64;
+      finalize = hardware_rand64_fini;
+    }
+    else {
+      fprintf(stderr, "No option supplied for input -> rdrand is default -> rdrand not supported.");
+      return 1;
+    }
+  }
+  else if (options.input == RDRAND) {
+    /* same code but different error */
+    if (rdrand_supported()) {
+      initialize = hardware_rand64_init;
+      rand64 = hardware_rand64;
+      finalize = hardware_rand64_fini;
+    }
+    else {
+      fprintf(stderr, "rdrand not supported.");
+      return 1;
+    }
+  }
+  else if (options.input == LRAND48_R) {
+    initialize = hardware_rand64_init;
+    rand64 = hardware_lrand48;
+    finalize = hardware_rand64_fini;
+  }
+  else if (options.input == FILE) {
+    read_file(options.rand_src_filename);
+    initialize = software_rand64_init;
+    rand64 = software_rand64;
+    finalize = software_rand64_fini;
+  }
+  else {
+    fprintf(stderr, "Input not detected as invalid, but could not determine randomness function.");
+    return 1;
+  }
+
+  initialize();
+  int output_word_size = sizeof rand64();
+  int output_errno = 0;
+
+  if (options.output == STDOUT) {
+    do {
+      unsigned long long computed_rand = rand64();
+      int output_bytes;
+      if (num_output_bytes < output_word_size) {
+        output_bytes = num_output_bytes;
+      }
+      else {
+        output_bytes = output_word_size;
+      }
+      if (!writebytes(computed_rand, output_bytes)) {
+        output_errno = errno;
+        break;
+      }
+      num_output_bytes -= output_bytes;
+    }
+    while (0 < num_output_bytes);
+
+    if (fclose(stdout) != 0) {
+      
+    }
+  }
   /*
   // Check arguments.
   bool valid = false;
