356cf2a485e0c202dfe9db4a3daf3488036d5aa0
Nick Bailey <nickbailey@g.ucla.edu>
2024-03-12 21:34:41 -0700

    added comments to test script

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# tests different options to make sure all are read correctly - would see errors in stderr if not
+# can't really check the actual output, so just verify that 100 bytes are output each time
 ./randall 100 -i lrand48_r | wc -c
 ./randall 100 -i /dev/full | wc -c
 ./randall 100 -i rdrand | wc -c
